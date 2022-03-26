dc4c9f9f2aefc8995a242948364820c65ca6af65
arturoalba <ajdalba@gmail.com>
2022-03-25 17:44:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Code your solutions in this file
+
+// code examples from canvas
+
+// for (let age = 30; age < 40; age++) {
+    // console.log(`I'm ${age} years old. Happy birthday to me!`);
+  
+// }
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+// 
+// function wrapGifts(gifts) {
+  // for (let i = 0; i < gifts.length; i++) {
+    // console.log(`Wrapped ${gifts[i]} and added a bow!`);
+     
+  // }
+ 
+  // return gifts;
+// }
+ 
+// wrapGifts(gifts);
+
+
+//  Thank you card creator function, Looping lab code Assignment #1
+
+// const stringName = ["Ada", "Brendan", "Ali"];
+// const eventName = "birthday";
+
+// function writeCards(stringName, eventName){
+    // for ( let i = 0; i < stringName.length; i++) {
+        // console.log([`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`]);
+        
+// }
+        // return newArray;
+
+// }
+
+// writeCards(["Ada","Brendan","Ali"], "birthday");
+
+
+// for loop and while loop example from canvas
+
+// let countup = 0;
+// while (countup < 10) {
+  // console.log(countup++);
+// }
+
+// 
+
+// for (let countup = 0; countup < 10; countup++) {
+  // console.log(countup);
+// }
+
+// 
+
+// Looping lab while loop code, Assignment #2
+// countDown to 0.
+
+let countDown = 10;    //  initialize variable
+    while (countDown >= 0) {     // if countDown (True) continue to run
+    console.log(countDown);  // logs the countDown
+    countDown--;    // decrease countDown by 1, updates countDown
+}
+
+
+
+
+
